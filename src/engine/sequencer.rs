fdcb1c9a024eecb20e3cdcdc0e021bcad207655f
John Purcell <john@caveofprogramming.com>
2025-12-01 15:52:40 +0100

    Added retrieving phrase data in sequencer

--- src/engine/sequencer.rs
+++ src/engine/sequencer.rs
@@ -1,7 +1,8 @@
 use crate::engine::audio::*;
 use crate::messaging::Action;
 use crate::types::PatternId;
-use crossbeam::channel::{Receiver, Sender};
+use crossbeam::channel::{Receiver, Sender, bounded};
+use std::thread;
 
 pub struct Sequencer {
     tx: Sender<Action>,
@@ -22,7 +23,28 @@ impl Sequencer {
         }
     }
 
-    pub fn run(&self) {}
+    pub fn run(&self) {
+        let rx = self.rx.clone();
+        let tx = self.tx.clone();
+
+        thread::spawn(move || {
+            while let Ok(action) = rx.recv() {
+                match action {
+                    Action::PlayPhrase(phrase_id) => {
+                        let (reply_tx, reply_rx) = bounded(1); // one-shot channel
+                        tx.send(Action::GetPhraseData {
+                            phrase_id,
+                            reply_to: reply_tx,
+                        })
+                        .unwrap();
+
+                        let phrase_data = reply_rx.recv().unwrap();
+                    }
+                    _ => {}
+                }
+            }
+        });
+    }
 
     pub fn tick(&self) {}
 }
